09800382c7cf90598af7768bb155630ea1d61fa4
nidkulkarni <133345557+nidkulkarni@users.noreply.github.com>
2024-08-08 14:09:20 +0530

    Add files via upload

---
+++ roboticcar.py
@@ -0,0 +1,77 @@
+from gpiozero import Motor
+import curses
+
+flmotor = Motor(forward=16, backward=17)
+frmotor = Motor(forward=18, backward=13)
+blmotor = Motor(forward=9, backward=11)
+brmotor = Motor(forward=10, backward=12)
+
+def left():
+#    print('Left ...')
+# To move the car in the left direction
+    flmotor.backward()
+    frmotor.forward()
+    blmotor.backward()
+    brmotor.forward()
+
+def right():
+#    print('Right ...')
+# To move the car in the right direction
+    flmotor.forward()
+    frmotor.backward()
+    blmotor.forward()
+    brmotor.backward()
+
+def forward():
+#    print('Forwarding ...')
+# To move the car in the forward direction
+    flmotor.forward()
+    frmotor.forward()
+    blmotor.forward()
+    brmotor.forward()
+
+def reverse():
+#    print('Reversing ...')
+# To reverse the direction of the car
+    flmotor.backward()
+    frmotor.backward()
+    blmotor.backward()
+    brmotor.backward()
+
+def stop():
+#    print('Stopping ...')
+# To stop the car
+    flmotor.stop()
+    frmotor.stop()
+    blmotor.stop()
+    brmotor.stop()
+
+actions = {
+    curses.KEY_UP:    forward,
+    curses.KEY_DOWN:  reverse,
+    curses.KEY_LEFT:  left,
+    curses.KEY_RIGHT: right,
+}
+
+def main(window):
+    next_key = None
+    while True:
+        curses.halfdelay(1)
+        if next_key is None:
+            key = window.getch()
+        else:
+            key = next_key
+            next_key = None
+        if key != -1:
+            # KEY PRESSED
+            curses.halfdelay(3)
+            action = actions.get(key)
+            if action is not None:
+                action()
+            next_key = key
+            while next_key == key:
+                next_key = window.getch()
+            # KEY RELEASED
+            stop()
+
+curses.wrapper(main)
